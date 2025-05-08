58dab2c805b28870dff83a492e47546aafff66a9
nutcas3 <mauricee423@gmail.com>
2025-05-08 20:52:43 +0300

    Add ShowAllExamples function to display Go language examples

---
+++ week1/calculator/macus/kendi.go
@@ -0,0 +1,16 @@
+package kendi
+
+import "fmt"
+
+// ShowAllExamples runs all the Go language examples
+func ShowAllExamples() {
+	fmt.Println("\n=== GO LANGUAGE EXAMPLES ===")
+	
+	// Show data types examples
+	ShowGoTypesExample()
+	
+	// Show control flow examples
+	ShowControlFlow()
+	
+	fmt.Println("\n=== END OF EXAMPLES ===")
+}
