35f89eaeb748ca9b06cf24bddc0768bc3855d87a
johneliud <johneliud4@gmail.com>
2025-05-09 23:24:24 +0300

    add support for 'addIterable' operation to handle multiple number inputs

--- week1/calculator/cmd/main.go
+++ week1/calculator/cmd/main.go
@@ -14,11 +14,11 @@ func main() {
 		return
 	}
 
-	fmt.Println("Welcome to calculator.\nChoose an operation: 'add', 'sub', 'mul', or 'div'.")
+	fmt.Println("Welcome to calculator.\nChoose an operation: 'add', 'addIterable', 'sub', 'mul', or 'div'.\nNote: 'addIterable' allows you to add multiple numbers at once.")
 
 	// The significance of the infinite loop at this point is to ensure the calculator runs even after a successful operation. Closes when the user exits.
 	for {
-		var operation string
+		var operation, digitCount string
 
 		// The infinite loop at this point ensures the operation persists until a valid operation is provided.
 		for {
@@ -26,10 +26,41 @@ func main() {
 			fmt.Scan(&operation)
 			operation = strings.ToLower(strings.TrimSpace(operation))
 
-			if operation == "add" || operation == "sub" || operation == "mul" || operation == "div" {
+			if operation == "add" || operation == "additerable" || operation == "sub" || operation == "mul" || operation == "div" {
+				if operation == "additerable" {
+					fmt.Print("How many numbers do you wish to input? ")
+					fmt.Scan(&digitCount)
+
+					count, err := strconv.Atoi(digitCount)
+					checkError(err)
+
+					num := make([]float64, 0, count)
+
+					for i := 0; i < int(count); i++ {
+						var input string
+
+						fmt.Printf("Enter input %d: ", i+1)
+						fmt.Scan(&input)
+
+						input_, loopErr := strconv.ParseFloat(input, 64)
+						checkError(loopErr)
+
+						num = append(num, input_)
+					}
+
+					// Used continue intead of return for this block to let the calculator skip the rest of the opration.
+					if len(num) == 0 {
+						fmt.Println("No numbers were provided for addition.")
+						continue
+					}
+					result, err := operations.AddIterable(num)
+					checkError(err)
+					fmt.Printf("Result: %v\n", result)
+					continue
+				}
 				break
 			}
-			fmt.Println("Invalid operation. Choose from 'add', 'sub', 'mul', or 'div'.")
+			fmt.Println("Invalid operation. Choose from 'add', 'addIterable', 'sub', 'mul', or 'div'.")
 		}
 
 		// Only proceeds to the next step if a valid number is provided by the user.
@@ -60,18 +91,32 @@ func main() {
 			fmt.Println("Invalid number. Try again.")
 		}
 
-		var result float64
+		var (
+			result float64
+			err    error
+		)
 
 		switch operation {
 		case "add":
-			result = operations.Add(num1, num2)
+			result, err = operations.Add(num1, num2)
+			checkError(err)
 		case "sub":
-			result = operations.Subtract(num1, num2)
+			result, err = operations.Subtract(num1, num2)
+			checkError(err)
 		case "mul":
-			result = operations.Multiply(num1, num2)
+			result, err = operations.Multiply(num1, num2)
+			checkError(err)
 		case "div":
-			result = operations.Divide(num1, num2)
+			result, err = operations.Divide(num1, num2)
+			checkError(err)
 		}
 		fmt.Printf("Result: %v\n", result)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Invalid number.")
+		return
 	}
 }
