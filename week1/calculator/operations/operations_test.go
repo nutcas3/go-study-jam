962c90b5736fb73255bb9d918a526e8c15538126
johneliud <johneliud4@gmail.com>
2025-05-09 20:51:16 +0300

    add unit tests for basic arithmetic operations: Add, Subtract, Multiply, and Divide

---
+++ week1/calculator/operations/operations_test.go
@@ -0,0 +1,83 @@
+package operations
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		x, y, z float64
+	}{
+		{2, 2, 4},
+		{-2, 2, 0},
+		{-2, -2, -4},
+	}
+
+	for _, tc := range tests {
+		got := Add(float64(tc.x), float64(tc.y))
+		expected := tc.z
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("Test failed. Performing addition of %v and %v got %v but expected %v", tc.x, tc.y, got, expected)
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	tests := []struct {
+		x, y, z float64
+	}{
+		{2, 2, 0},
+		{-2, 2, -4},
+		{-2, -2, 0},
+	}
+
+	for _, tc := range tests {
+		got := Subtract(float64(tc.x), float64(tc.y))
+		expected := tc.z
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("Test failed. Performing subtraction of %v and %v got %v but expected %v", tc.x, tc.y, got, expected)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	tests := []struct {
+		x, y, z float64
+	}{
+		{2, 2, 4},
+		{-2, 2, -4},
+		{-2, -2, 4},
+	}
+
+	for _, tc := range tests {
+		got := Multiply(float64(tc.x), float64(tc.y))
+		expected := tc.z
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("Test failed. Performing multiplication of %v and %v got %v but expected %v", tc.x, tc.y, got, expected)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	tests := []struct {
+		x, y, z float64
+	}{
+		{2, 2, 1},
+		{-2, 2, -1},
+		{-2, -2, 1},
+		{2, 0, 0},
+	}
+
+	for _, tc := range tests {
+		got := Divide(float64(tc.x), float64(tc.y))
+		expected := tc.z
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("Test failed. Performing division of %v and %v got %v but expected %v", tc.x, tc.y, got, expected)
+		}
+	}
+}
